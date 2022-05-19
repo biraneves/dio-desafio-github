f9fc32e845549f4e850eae00752148a12339ae46
Ubirajara Neves <ubirajara.neves@usp.br>
2022-05-19 15:26:09 -0300

    ReactJS - Webpack

---
+++ Introdução ao ReactJS/lesson-05/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+    return (
+        <div></div>
+    );
+
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
