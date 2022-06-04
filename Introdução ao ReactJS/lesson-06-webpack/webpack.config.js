23a798615a2d88aedd8e36407fc6a30d70a6d1ac
biraneves <ubirajara.neves@usp.br>
2022-06-04 10:59:47 -0300

    Exercícios com webpack

--- Introdução ao ReactJS/lesson-06-webpack/webpack.config.js
+++ Introdução ao ReactJS/lesson-06-webpack/webpack.config.js
@@ -4,6 +4,17 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundler.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\;(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 };
